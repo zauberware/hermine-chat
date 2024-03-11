12a69a9d5d072c6eeb088160991915c9df18271a
Christoph Kahl <christoph@zauberware.com>
2024-03-11 10:34:46 +0100

    added momentjs, formatted dates of messages

--- src/components/chatMessage/ChatMessage.tsx
+++ src/components/chatMessage/ChatMessage.tsx
@@ -1,13 +1,39 @@
+import moment from "moment";
 import React from "react";
 import { useSettings } from "../../context";
 import { getLogoUrl } from "../../utils";
 import "./ChatMessage.css";
 import { ChatMessageProps } from "./ChatMessage.types";
 
+moment.updateLocale('de', {
+    relativeTime : {
+        future: "in %s",
+        past:   "vor %s",
+        s  : 'vor einem Augenblick',
+        ss : '%d Sekunden',
+        m:  "einer Minute",
+        mm: "%d Minuten",
+        h:  "einer Stunde",
+        hh: "%d Stunden",
+        d:  "einem Tag",
+        dd: "%d Tagen",
+        w:  "einer Woche",
+        ww: "%d Wochen",
+        M:  "einem Monat",
+        MM: "%d Monaten",
+        y:  "einem Jahr",
+        yy: "%d Jahren"
+    }
+});
+
 const ChatMessage: React.FC<ChatMessageProps> = ({ message, lastMessage }) => {
   const { theme } = useSettings();
   const renderAiMessage = () => (
-    <div ref={lastMessage} id={message.id} className="flex w-full mt-2 space-x-3 max-w-xs">
+    <div
+      ref={lastMessage}
+      id={message.id}
+      className="flex w-full mt-2 space-x-3 max-w-xs"
+    >
       <div
         style={{ backgroundImage: `url(${getLogoUrl(theme?.ai_icon)})` }}
         className="flex-shrink-0 h-10 w-10 rounded-ful border-gray-300 border bg-contain rounded-full"
@@ -16,9 +42,9 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ message, lastMessage }) => {
         <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
           <p className="text-sm">{message.result}</p>
         </div>
-        <span className="text-xs text-gray-500 leading-none">
-          {new Date(message.updated_at).toLocaleTimeString()}
-        </span>
+        <div className="text-xs text-gray-500 leading-none pt-1 pb-2">
+          {moment(message.updated_at).fromNow()}
+        </div>
       </div>
     </div>
   );
@@ -33,9 +59,9 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ message, lastMessage }) => {
         <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
           <p className="text-sm">{message.result}</p>
         </div>
-        <span className="text-xs text-gray-500 leading-none">
-          {new Date(message.updated_at).toLocaleTimeString()}
-        </span>
+        <div className="text-xs text-gray-500 leading-none text-end pt-1 pb-2">
+          {moment(message.updated_at).fromNow()}
+        </div>
       </div>
       <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
     </div>
