5c32bec4aaa45fb475469e02eb92875c89182046
Christoph Kahl <christoph@zauberware.com>
2024-03-12 15:49:57 +0100

    added logoUrl if given as logo

--- src/components/floatingButton/FloatingButton.tsx
+++ src/components/floatingButton/FloatingButton.tsx
@@ -3,17 +3,24 @@ import "./FloatingButton.css";
 import { FloatingButtonProps } from "./FloatingButton.types";
 import Icon from "../../assets/images/logo.svg";
 import { useSettings } from "../../context";
+import { getLogoUrl } from "../../utils";
 
-const FloatingButton: React.FC<FloatingButtonProps> = ({
-  setToggled,
-}) => {
-  const { settings } = useSettings()
+const FloatingButton: React.FC<FloatingButtonProps> = ({ setToggled }) => {
+  const { settings, theme } = useSettings();
+  let logoUrl;
+  if (theme.logo) {
+    logoUrl = getLogoUrl(theme.logo);
+  }
   return (
     <button
       onClick={() => setToggled((t) => !t)}
       className={`floating-button floating-button-${settings.location}`}
+      style={logoUrl ? {
+        backgroundImage: `url(${logoUrl})`,
+        backgroundSize: "cover",
+      } : {}}
     >
-      <Icon height={50} width={40} />
+      {!logoUrl ? <Icon height={50} width={40} /> : null}
     </button>
   );
 };
