d545fca3eab48eb8e0c86480cd5270bacb20cd16
Christoph Kahl <christoph@zauberware.com>
2024-04-17 13:14:10 +0200

    only init sentry if not already initialized

--- src/index.tsx
+++ src/index.tsx
@@ -8,27 +8,29 @@ import "./utils/i18n";
 import packageJson from "../package.json";
 
 const HermineChat = (settings: ISettings) => {
-  Sentry.init({
-    dsn: "https://0792746c0b33b1c795864f17e7fefa45@sentry.zauberware.com/32",
-    replaysOnErrorSampleRate: 1.0,
-    replaysSessionSampleRate: 0.5,
-    sampleRate: 0.5,
-    enableTracing: true,
-    release: packageJson.version,
-    tracePropagationTargets: [/hermine.ai/, /localhost/],
-    integrations: [
-      new Sentry.BrowserTracing({
-        traceFetch: false,
-        traceXHR: true,
-        tracePropagationTargets: [/hermine.ai/, /localhost/],
-      }),
-      new Sentry.Replay({
-        // Additional SDK configuration goes in here, for example:
-        maskAllText: false,
-        blockAllMedia: false,
-      }),
-    ],
-  });
+  if (!Sentry.getCurrentHub().getClient()) {
+    Sentry.init({
+      dsn: "https://0792746c0b33b1c795864f17e7fefa45@sentry.zauberware.com/32",
+      replaysOnErrorSampleRate: 1.0,
+      replaysSessionSampleRate: 0.5,
+      sampleRate: 0.5,
+      enableTracing: true,
+      release: packageJson.version,
+      tracePropagationTargets: [/hermine.ai/, /localhost/],
+      integrations: [
+        new Sentry.BrowserTracing({
+          traceFetch: false,
+          traceXHR: true,
+          tracePropagationTargets: [/hermine.ai/, /localhost/],
+        }),
+        new Sentry.Replay({
+          // Additional SDK configuration goes in here, for example:
+          maskAllText: false,
+          blockAllMedia: false,
+        }),
+      ],
+    });
+  }
   const defaultSettings = {
     location: "bottom",
     chatTitle: "KI-Chat",
